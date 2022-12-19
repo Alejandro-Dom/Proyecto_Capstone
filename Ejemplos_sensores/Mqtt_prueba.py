90ecd44ff67d7a4748e45ef38a30e2c0fdf00898
Luis Manuel Sánchez Vivar <106187407+ManuSV16@users.noreply.github.com>
2022-12-18 21:41:02 -0600

    Add files via upload

---
+++ Ejemplos_sensores/Mqtt_prueba.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+pin=""
+def on_connect(client, userdata,flags,rc):
+    print("Se conecto con mqtt" + str(rc))
+    client.subscribe("Capstone/Caja_Seguridad_Biometrica/MADS")
+def on_message(client, userdata, msg):
+    if msg.topic == "Capstone/Caja_Seguridad_Biometrica/MADS":
+        pin=((msg.payload))
+        print (pin)
+client=mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("broker.hivemq.com",1883,60)
+client.loop_forever()
