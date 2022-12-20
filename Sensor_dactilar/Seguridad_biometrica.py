325bd5633da770f4f51d7fb3e94a7d216101cc7d
Alejandro-Dom <alejandrodomra@gmail.com>
2022-12-19 20:54:38 -0600

    Update Seguridad_biometrica.py

--- Sensor_dactilar/Seguridad_biometrica.py
+++ Sensor_dactilar/Seguridad_biometrica.py
@@ -87,36 +87,33 @@ client.connect("broker.hivemq.com",1883,60)
 client.loop_start()
 time.sleep(3)
 
-
-try:
-    print("Ingresa la contraseña")
-    print(keypad)
-    print("Ponga su dedo sobre el escaner")
-    if get_fingerprint():
-        print("Huella detectada con ID #", finger.finger_id, "con valor de confianza =", finger.confidence)
-        GPIO.output(LEDV, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LEDV, GPIO.LOW)
-        print("Ingresa la contraseña")
-        enviarmqtt("Capstone/Caja_Seguridad_Biometrica/MADS/Confirmacion","Truehuella")
-
-    else:
-        print("Huella no encontrada")
-        GPIO.output(LEDR, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LEDR, GPIO.LOW)
-        while True:
-            GPIO.output(buzz, GPIO.HIGH)
-            sleep(0.3)
-            GPIO.output(buzz, GPIO.LOW)
-            sleep(0.3) 
-    GPIO.cleanup()
-    p.stop()
-    raise SystemExit
-except KeyboardInterrupt:         
-    print("Adiós")
-    p.stop()
-    GPIO.cleanup()
-    client.loop_stop()
-    client.disconnect()
-    raise SystemExit
+print("Ingresa la contraseña")
+while keypad == "True":
+    try:
+        print("Ponga su dedo sobre el escaner")
+        if get_fingerprint():
+            print("Huella detectada con ID #", finger.finger_id, "con valor de confianza =", finger.confidence)
+            GPIO.output(LEDV, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(LEDV, GPIO.LOW)
+            enviarmqtt("Capstone/Caja_Seguridad_Biometrica/MADS/Confirmacion","Truehuella")
+        else:
+            print("Huella no encontrada")
+            GPIO.output(LEDR, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(LEDR, GPIO.LOW)
+            while True:
+                GPIO.output(buzz, GPIO.HIGH)
+                sleep(0.3)
+                GPIO.output(buzz, GPIO.LOW)
+                sleep(0.3) 
+        GPIO.cleanup()
+        p.stop()
+        raise SystemExit
+    except KeyboardInterrupt:         
+        print("Adiós")
+        p.stop()
+        GPIO.cleanup()
+        client.loop_stop()
+        client.disconnect()
+        raise SystemExit
