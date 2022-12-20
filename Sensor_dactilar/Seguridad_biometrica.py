1f243029ac7606c2cae5d2d6412a1452fea8c6bb
Alejandro-Dom <alejandrodomra@gmail.com>
2022-12-19 20:56:58 -0600

    Update Seguridad_biometrica.py

--- Sensor_dactilar/Seguridad_biometrica.py
+++ Sensor_dactilar/Seguridad_biometrica.py
@@ -32,6 +32,7 @@ GPIO.setup(servo, GPIO.OUT)
 GPIO.setup(buzz, GPIO.OUT, initial = GPIO.LOW)
 p = GPIO.PWM(servo,50) #GPIO 17 para PWM con pulso de 50 Hz
 p.start(2.5)
+keypad = ""
 
 # Usando con Linux/Raspberry Pi 4 y hardware UART:
 uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
